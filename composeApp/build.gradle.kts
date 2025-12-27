67450bb5d775e0f5bdc6e88d3b56c74d709c6a16
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-12-27 16:37:35 -0500

    feat: integrate Koin KSP for annotation processing

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.google.protobuf)
     alias(libs.plugins.squareup.wire)
+    alias(libs.plugins.google.ksp)
 }
 
 kotlin {
@@ -55,12 +56,18 @@ kotlin {
             implementation(libs.io.koin.compose)
             implementation(libs.io.koin.compose.viewmodel)
             implementation(libs.io.koin.compose.viewmodel.navigation)
+            api(libs.io.koin.annotations)
             // DataStore
             implementation(libs.androidx.datastore.core.okio)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
+    }
+
+    // KSP Common sourceSet
+    sourceSets.named("commonMain").configure {
+        kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
     }
 }
 
@@ -99,6 +106,19 @@ wire {
     }
 }
 
+ksp {
+    arg("KOIN_GENERATION_PACKAGE", "com.yoimerdr.compose.ludens.koin.generated")
+}
+
 dependencies {
     debugImplementation(compose.uiTooling)
+    add("kspCommonMainMetadata", libs.io.koin.ksp.compiler)
+    add("kspAndroid", libs.io.koin.ksp.compiler)
+    add("kspIosArm64", libs.io.koin.ksp.compiler)
+    add("kspIosSimulatorArm64", libs.io.koin.ksp.compiler)
+}
+
+// Trigger Common Metadata Generation from Native tasks
+tasks.matching { it.name.startsWith("ksp") && it.name != "kspCommonMainKotlinMetadata" }.configureEach {
+    dependsOn("kspCommonMainKotlinMetadata")
 }
--- composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/di/Koin.kt
@@ -3,25 +3,25 @@ package com.yoimerdr.compose.ludens.app.di
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import org.koin.compose.KoinMultiplatformApplication
-import org.koin.core.KoinApplication
 import org.koin.core.annotation.KoinExperimentalAPI
-import org.koin.core.module.Module
+import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.koinConfiguration
 
 @OptIn(KoinExperimentalAPI::class)
 @Composable
 fun KoinInjection(
-    additionalModules: (KoinApplication.() -> List<Module>)? = null,
+    configuration: KoinAppDeclaration? = null,
     content: @Composable () -> Unit,
 ) {
-    val configuration = remember(additionalModules) {
+    val config = remember(configuration) {
         koinConfiguration {
-            modules(ApplicationModules + (additionalModules?.invoke(this) ?: emptyList()))
+            configuration?.invoke(this)
+            modules(ApplicationModules)
         }
     }
 
     KoinMultiplatformApplication(
-        config = configuration,
+        config = config,
         content = content
     )
 }
