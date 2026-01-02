1105742ff2ad6c3069737727eb8853d164967240
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-02 17:28:20 -0500

    feat: add Android-specific implementations for data store and platform context

---
+++ composeApp/src/androidMain/kotlin/com/yoimerdr/compose/ludens/core/di/CoreModule.android.kt
@@ -0,0 +1,26 @@
+package com.yoimerdr.compose.ludens.core.di
+
+import androidx.datastore.core.DataStore
+import com.yoimerdr.compose.ludens.core.data.source.local.createDataStore
+import com.yoimerdr.compose.ludens.core.data.source.local.settings.ProtoSettings
+import com.yoimerdr.compose.ludens.core.infrastructure.platform.AndroidPlatformApplication
+import com.yoimerdr.compose.ludens.core.infrastructure.platform.AndroidPlatformContext
+import com.yoimerdr.compose.ludens.core.infrastructure.platform.PlatformApplication
+import com.yoimerdr.compose.ludens.core.infrastructure.platform.PlatformContext
+import okio.FileSystem
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.scope.Scope
+
+
+actual fun Scope.createDataStore(): DataStore<ProtoSettings> = createDataStore(FileSystem.SYSTEM) {
+    androidContext().filesDir
+        .resolve(dataStoreFilename)
+        .absolutePath
+}
+
+actual fun Scope.createPlatformContext(): PlatformContext {
+    return AndroidPlatformContext(androidContext())
+}
+
+actual fun Scope.createPlatformApplication(): PlatformApplication =
+    AndroidPlatformApplication(get())
