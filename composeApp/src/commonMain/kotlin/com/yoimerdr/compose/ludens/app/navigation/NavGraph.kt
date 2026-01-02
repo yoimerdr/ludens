058096218199949749d8364028af2da0794d56c5
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-02 18:02:01 -0500

    feat: enhance navigation graph with SplashScreen and features management

--- composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/navigation/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/navigation/NavGraph.kt
@@ -1,14 +1,23 @@
 package com.yoimerdr.compose.ludens.app.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.yoimerdr.compose.ludens.app.ui.screens.SplashScreen
+import com.yoimerdr.compose.ludens.ui.state.WebFeaturesState
+import com.yoimerdr.compose.ludens.ui.state.WebFeaturesStateSaver
 
 @Composable
 fun NavGraph(
     navController: NavHostController,
 ) {
+    val features = rememberSaveable(stateSaver = WebFeaturesStateSaver) {
+        mutableStateOf(WebFeaturesState())
+    }
+
     NavHost(
         navController = navController,
         startDestination = Destination.Splash.route,
@@ -17,23 +26,20 @@ fun NavGraph(
             route = Destination.Splash.route,
             arguments = Destination.Splash.arguments
         ) {
-
+            SplashScreen(navController) {
+                features.value = it
+            }
         }
 
         composable(
             route = Destination.Home.route,
             arguments = Destination.Home.arguments,
-        ) {
-        }
+        ) {}
 
         composable(
             route = Destination.Settings.route,
             arguments = Destination.Settings.arguments,
-        ) {
-
-        }
+        ) {}
     }
 
 }
-
-
