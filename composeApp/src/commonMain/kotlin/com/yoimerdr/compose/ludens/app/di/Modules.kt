4303ce8af73b4dab0f457111778f858b2737e54a
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-02 18:01:47 -0500

    feat: integrate CoreModule

--- composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/di/Modules.kt
@@ -1,7 +1,9 @@
 package com.yoimerdr.compose.ludens.app.di
 
+import com.yoimerdr.compose.ludens.core.di.CoreModule
+import com.yoimerdr.compose.ludens.koin.generated.module
 import org.koin.core.module.Module
 
 val ApplicationModules: List<Module> = listOf(
-
+    CoreModule.module
 )
