ec53c051a95f7c5b2d71aa5267cb5f38383152d3
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-03 21:31:22 -0500

    feat: add Koin module for Home feature dependency injection

---
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/features/home/di/HomeModule.kt
@@ -0,0 +1,10 @@
+package com.yoimerdr.compose.ludens.features.home.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
+import org.koin.core.annotation.Module
+
+@Module
+@Configuration
+@ComponentScan("com.yoimerdr.compose.ludens.features.home")
+object HomeModule
