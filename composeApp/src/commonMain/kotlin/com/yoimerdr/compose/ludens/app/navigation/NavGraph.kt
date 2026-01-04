c932084762f2222578e2ea8c516a609f4c28c5aa
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-03 21:32:04 -0500

    feat: integrate HomeModule into application modules and navigation with HomeScreen

--- composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/navigation/NavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/navigation/NavGraph.kt
@@ -1,12 +1,20 @@
 package com.yoimerdr.compose.ludens.app.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.layer.GraphicsLayer
+import androidx.compose.ui.graphics.rememberGraphicsLayer
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.yoimerdr.compose.ludens.app.ui.screens.SplashScreen
+import com.yoimerdr.compose.ludens.features.home.presentation.screen.HomeScreen
+import com.yoimerdr.compose.ludens.ui.state.PluginState
+import com.yoimerdr.compose.ludens.ui.state.PluginStateSaver
 import com.yoimerdr.compose.ludens.ui.state.WebFeaturesState
 import com.yoimerdr.compose.ludens.ui.state.WebFeaturesStateSaver
 
@@ -14,9 +22,24 @@ import com.yoimerdr.compose.ludens.ui.state.WebFeaturesStateSaver
 fun NavGraph(
     navController: NavHostController,
 ) {
-    val features = rememberSaveable(stateSaver = WebFeaturesStateSaver) {
+    var features by rememberSaveable(stateSaver = WebFeaturesStateSaver) {
         mutableStateOf(WebFeaturesState())
     }
+
+    var plugin by rememberSaveable(stateSaver = PluginStateSaver) {
+        mutableStateOf(PluginState())
+    }
+
+    val webGraphicsLayer = rememberGraphicsLayer()
+
+    NavHomeGraph(
+        nav = navController,
+        features = features,
+        webGraphicsLayer = webGraphicsLayer,
+    ) {
+        plugin = it
+    }
+
 
     NavHost(
         navController = navController,
@@ -27,7 +50,7 @@ fun NavGraph(
             arguments = Destination.Splash.arguments
         ) {
             SplashScreen(navController) {
-                features.value = it
+                features = it
             }
         }
 
@@ -42,4 +65,24 @@ fun NavGraph(
         ) {}
     }
 
+}
+
+
+@Composable
+private fun NavHomeGraph(
+    nav: NavHostController,
+    features: WebFeaturesState,
+    webGraphicsLayer: GraphicsLayer,
+    onLoad: ((PluginState) -> Unit)? = null,
+) {
+    val currentRoute = nav.currentBackStackEntryAsState().value?.destination?.route
+    val show = currentRoute != null && currentRoute != Destination.Splash.route
+    if (!show) return
+
+    HomeScreen(
+        nav = nav,
+        features = features,
+        webGraphicsLayer = webGraphicsLayer,
+        onLoad = onLoad,
+    )
 }
