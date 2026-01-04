9f242be0838c8c09bf6dd96ef8ad3024b675bc5e
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-03 20:46:32 -0500

    feat: add HomeEvent sealed interface for handling home screen events

---
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/features/home/presentation/state/HomeEvent.kt
@@ -0,0 +1,8 @@
+package com.yoimerdr.compose.ludens.features.home.presentation.state
+
+/**
+ * Sealed interface representing events that can occur in the home screen.
+ */
+sealed interface HomeEvent {
+
+}
