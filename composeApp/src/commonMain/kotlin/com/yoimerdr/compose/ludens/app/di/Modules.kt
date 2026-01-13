b5ea65b97af9fc6d290c47ff84d27fc4bfdebc5c
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-12 19:56:31 -0500

    feat: add settings module to dependency injection

--- composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/di/Modules.kt
@@ -1,9 +1,11 @@
 package com.yoimerdr.compose.ludens.app.di
 
 import com.yoimerdr.compose.ludens.core.di.CoreModule
+import com.yoimerdr.compose.ludens.features.settings.di.SettingsModule
 import com.yoimerdr.compose.ludens.koin.generated.module
 import org.koin.core.module.Module
 
 val ApplicationModules: List<Module> = listOf(
-    CoreModule.module
+    CoreModule.module,
+    SettingsModule.module
 )
