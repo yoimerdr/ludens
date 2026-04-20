18493c83ff548443cc3fe2528418fca71a8fdc35
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-04-20 12:49:36 -0500

    feat(build): create build logic module

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    compileOnly(libs.android.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.compose.gradle.plugin)
+    compileOnly(libs.ksp.gradle.plugin)
+}
+
+gradlePlugin {
+    plugins {
+
+    }
+}
